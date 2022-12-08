6ac20a012a7434bd17e87518a51a7627865c22d6
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-08 16:36:40 +0200

    router explaination

--- introToNode/node-server-demo-3-master/router.js
+++ introToNode/node-server-demo-3-master/router.js
@@ -9,15 +9,16 @@ function register(method, path, handler) {
     routes[path][method] = handler;
 }
 
+// req, res like e in eventListener => no need to put them in match in index.js but here useful, their info comes from client
 function match(req, res) {
     console.log('>>>', req.method, req.url);
 
-    const url = new URL(req.url, `http://${req.headers.host}`);
+    const url = new URL(req.url, `http://${req.headers.host}`); //needs base too so two args
 
     let handler;
     const actions = routes[url.pathname];
     if (actions != undefined) {
-        handler = actions[req.method];
+        handler = actions[req.method]; // handler is basically routes[path][method], a bit confusing because handler = routes[path][method]; which in routes object = handler
     }
 
     if (typeof handler == 'function') {
@@ -29,7 +30,7 @@ function match(req, res) {
 
 module.exports = {
     register,
-    get: register.bind(null, 'GET'),
-    post: register.bind(null, 'POST'),
+    get: register.bind(null, 'GET'), //partial application
+    post: register.bind(null, 'POST'), //partial application
     match
 };
