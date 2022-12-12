1f3dc0706c20152f5797366e66b7dbda4b7b239a
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-12 16:30:50 +0200

    unsubscribe logic

--- nodeStreams/pubsub/demo.js
+++ nodeStreams/pubsub/demo.js
@@ -1,22 +1,24 @@
 const eventBus = require('./eventBus');
 
+// first, we use the initial functionality of subscribe -> register events
+// second, we store subscribe in a function expression to make use of functionality after return in subscribe definition
+// then, we execute subscribes and one publish, result is according to initial subscribe functionality
+// BUT, we execute the function expression storing the secondary subscribe functionlaity below return which filters out the callback specified in this case the first one with two names so only the second callback with one name persists
+// then, the effects of the filter take place and the event say-hello's first callback is deactivated
+
 // subscribe events into a closure object ready to be emitted
-eventBus.subscribe('say-hello', (name, secondName) => console.log('event say-hello executed! - ' + name + ' ' + secondName));
+let firstSayHelloUnsubsrcibe = eventBus.subscribe('say-hello', (name, secondName) => console.log('event say-hello executed! - ' + name + ' ' + secondName));
 eventBus.subscribe('say-hello', (name, secondName) => console.log('event say-hello executed! Second time - ' + name + ' ' + secondName));
 eventBus.subscribe('say-bye', (name) => console.log('event say-bye executed! - ' + name));
 
 // publish/emit the data subscribed to the closure object
-eventBus.publish('say-hello', 'Pesho')
 eventBus.publish('say-hello', 'Gosho', 'Ivan')
+
+firstSayHelloUnsubsrcibe();
+
+eventBus.publish('say-hello', 'Pesho')
 eventBus.publish('say-bye', 'Pesho')
 
 // not only emitting but also data is passed as an arg to make emit dynamic
-
-// >> 
-// event say-hello executed! - Pesho undefined
-// event say-hello executed! Second time - Pesho undefined  
-// event say-hello executed! - Gosho Ivan
-// event say-hello executed! Second time - Gosho Ivan       
-// event say-bye executed! - Pesho
 
 // because first publish is with Pesho as only argument while app expects name and secondName
--- nodeStreams/pubsub/eventBus.js
+++ nodeStreams/pubsub/eventBus.js
@@ -5,11 +5,19 @@ const subscribers = {};
 // }
 
 // addEventListener
+// subscribe becomes a higher-order function 
+// => manipulate data via code above to return
+// => after that return another function with different functionlaity
+// subscribe() has dual funcionality -> initial that sets some conditions and secondary whose functionality is separate 
+
 function subscribe(eventType, callback){
     if(!subscribers[eventType]){
         subscribers[eventType] = [];
     }
     subscribers[eventType].push(callback);
+    return () => {
+        subscribers[eventType] = subscribers[eventType].filter(x => x != callback) //compare functions by virtue of functions being stored as reference 
+    }
 }
 
 // emit
@@ -22,3 +30,4 @@ module.exports = {
     subscribe,
     publish
 }
+
