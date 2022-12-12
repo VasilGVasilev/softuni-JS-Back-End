6b257405fb768f77b4258a447a2fe6e83a49b0ad
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-12 17:39:43 +0200

    eventEmitter update

--- nodeStreams/pubsub/events/eventBusWithEvents.js
+++ nodeStreams/pubsub/events/eventBusWithEvents.js
@@ -1,34 +1,13 @@
-const events = require('events');
-let eventEmitter = new events.EventEmitter();
+const EventEmitter = require('events');
+let eventEmitter = new EventEmitter();
 
-const subscribers = {};
-
-
-// function subscribe(eventType, callback){
-//     if(!subscribers[eventType]){
-//         subscribers[eventType] = [];
-//     }
-//     subscribers[eventType].push(callback);
-//     return () => {
-//         subscribers[eventType] = subscribers[eventType].filter(x => x != callback) //compare functions by virtue of functions being stored as reference 
-//     }
-// }
-
+// subscribe
 eventEmitter.on('click', (a, b) => {
     console.log('A click has been detected!');
     console.log(a + ' ' + b); //outputs 'Hello world'
 })
 
 
-
-// function publish(eventType, ...params){
-//     subscribers[eventType].forEach(cb => cb.apply(null, params)); //or cb(...params)
-// }
-
+// publish
 eventEmitter.emit('click', 'Hello', 'world');
-
-module.exports = {
-    subscribe,
-    publish
-}
 
