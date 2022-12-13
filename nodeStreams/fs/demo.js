98f31494394a231aab6c2949f2312c1fa1e34796
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-13 16:36:45 +0200

    fs readFile async

---
+++ nodeStreams/fs/demo.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+
+// better async rather then sync version
+fs.readFile('./text.txt', {encoding: 'utf-8'}, (err, data) =>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
+
+// end is first due to fs.readFile being async
+console.log('end');
