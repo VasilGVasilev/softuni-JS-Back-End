96916d5cd7f7212de271eef73dee1b721a220104
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-13 11:28:57 +0200

    encoding option to make buffer readable

--- nodeStreams/streams/firstDemo/firstDemo.js
+++ nodeStreams/streams/firstDemo/firstDemo.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./largeFile.txt');
+const readStream = fs.createReadStream('./text.txt', {encoding: 'utf-8'}); //options arg -> encoding so that you dont get the default buffer <Buffer 20 69 6d 70 65 72 64 69 65 74 20 64 75 69 20 61 63 63 75 6d 73 61 6e 20 73 69 74 2e 20 50 72 65 74 69 75 6d 20 76 75 6c 70 75 74 61 74 65 20 73 61 70 ... 21102 more bytes>
 // the moment data is read, an event is emitted
 
 // which here is caught with .on('data', cb)
