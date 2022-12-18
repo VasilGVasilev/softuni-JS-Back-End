3cd4645491a5972bcac8deb930618fcf437d4f6c
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-18 13:01:07 +0200

    res.send

--- introToExpress/demo/index.js
+++ introToExpress/demo/index.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express() //factory function that creates a new instance -> app === server in http.createServer
 
 app.get('/', (req, res) => {
-    res.write('Hello world');
-    res.end();
+    res.send('hello world') //instead res.write() + res.end();
 });
 
 app.listen(5000, ()=> console.log('Server listening on port 5000...'));
