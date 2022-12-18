79128f531971a839218cb2e9cbe1fc3a25585136
Vasil Vasilev <vaskomeil@gmail.com>
2022-12-18 13:37:40 +0200

    action on server-side awaiting requests

--- introToExpress/demo/index.js
+++ introToExpress/demo/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express() //factory function that creates a new instance -> app === server in http.createServer
 
+// Action - the entity on the server side that awaits the endpoint ('/') to be requested via GET to respond with the CB (req, res)
 app.get('/', (req, res) => {
-    res.send('hello world') //instead res.write() + res.end();
+    res.send('hello world') //instead res.write() + res.end(); Express is another layer on top of http, so these work but are too low-level
 });
 
 app.listen(5000, ()=> console.log('Server listening on port 5000...'));
