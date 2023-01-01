3dc0d9d8d285267ad42c2da3aed6493dc403248e
Vasil Vasilev <vaskomeil@gmail.com>
2023-01-01 15:33:21 +0200

    res.render(view, cb) explained

--- workshopExpress/Papazov Demo/src/controllers/homeController.js
+++ workshopExpress/Papazov Demo/src/controllers/homeController.js
@@ -7,6 +7,13 @@ router.get('/', (req, res) => {
     const cubes = cubeService.getAll(search, from, to);
 
     res.render('index', { cubes, search, from, to });
+    // res.render(view [, locals] [, callback])
+        
+        // The view argument is a string that is the file path of the view file to render. 
+        // This can be an absolute path, or a path relative to the views setting. -> app.set('views', './src/views') WE HAVE SET THE HBS TEMPLATE ENGINE, SO IT SEARCHES 'index' in ./src/views
+        // If the path does not contain a file extension, then the view engine setting determines the file extension. 
+        // If the path does contain a file extension, then Express will load the module for the specified template engine (via require()) and 
+        // render it using the loaded module’s __express function.
 });
 
 router.get('/about', (req, res) => {
