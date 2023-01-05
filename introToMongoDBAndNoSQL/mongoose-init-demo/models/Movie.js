325490f39a8bacdf34264a7943f7c06cdad0dd88
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-05 16:03:41 +0200

    mongoose Schema tolerates irregular documents

--- introToMongoDBAndNoSQL/mongoose-init-demo/models/Movie.js
+++ introToMongoDBAndNoSQL/mongoose-init-demo/models/Movie.js
@@ -8,12 +8,13 @@ const movieSchema = new mongoose.Schema({
         minlength: 2,
 
     },
-    description: String,
+    description: String, //this field is not required
     year: {
         type: Number,
         min: [1888, 'The year {VALUE} should not be higher than 1888'],
         required: false,
     },
+    // the Schema is not set to these three fields only, it is a non-relational DB, the POST request may add a field and an irregular document be created in the collection
 });
 
 // create a model of the schema
