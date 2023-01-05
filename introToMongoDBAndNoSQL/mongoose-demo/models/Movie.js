337ce3820928485e5ebbbc3add61d8c5aaafee42
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-05 18:13:35 +0200

    getInfo() method

--- introToMongoDBAndNoSQL/mongoose-demo/models/Movie.js
+++ introToMongoDBAndNoSQL/mongoose-demo/models/Movie.js
@@ -18,23 +18,40 @@ const movieSchema = new mongoose.Schema({
     imageUrl: String,
     year: {
         type: Number,
-        min: [1888, 'The year {VALUE} should not be higher than 1888'],
+        min: [1888, 'The year {VALUE} should not be higher than 1888'], //built-in validator
         required: false,
     },
 });
 
-// movieSchema.method('getInfo', function() {
-//     return `${this.title} - ${this.description || 'n/a'}`;
-// });
+
+
+// add method ! not persisted on DB
 movieSchema.methods.getInfo = function() {
     return `${this.title} - ${this.description || 'n/a'}`;
 };
 
+// or 
+// movieSchema.method('getInfo', function() {
+//     return `${this.title} - ${this.description || 'n/a'}`;
+// });
+// use function expression because it perists the context to use 'this'
+
+    // see console.log(movie.getInfo()); in movieController, now commented out
+
+    // log when create new movie:
+
+    // Iron Man - n/a
+    // Batman - n/a
+    // Third Test - some description here
+
+
+// a piece of data that is not persisted on DB but is accessible
 movieSchema.virtual('isNew')
     .get(function() {
         return this.year >= 2020
     });
 
+// complex validator
 movieSchema.path('title').validate(function() {
     return this.title.length >= 2 && this.title.length <= 20;
 }, `Movie title should be less than 20 characters and more than 2!`);
