cb1e4dee627deb41b62625636a83e8242658e7ef
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-08 17:39:13 +0200

    logic behind filter

--- workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
+++ workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
@@ -3,6 +3,9 @@ const Accessory = require('../models/Accessory');
 exports.getAll = () => Accessory.find();
 // {$nin: ids} is an operator  -> Matches none of the values specified in an array.
 // give me Id that are not part of the ids that are given as a param
+// initially the Cube does not have any ids of accessories, you attach them and now it has one, so display all that are not,
+// which is the one currently attached, then attach a second and out of three, two are in the Cube's accessories array, 
+// only one left is possible for display
 exports.getAllAvailable = (ids) => Accessory.find({_id: {$nin: ids}});
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
