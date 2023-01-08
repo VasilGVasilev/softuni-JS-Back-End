854952e3f161417c9aab284495e2d7669f5cdfc1
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-08 17:29:45 +0200

    filter accessories once used

--- workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
+++ workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
@@ -1,7 +1,7 @@
 const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
-
+// {$nin: ids} is an operator  -> Matches none of the values specified in an array.
 exports.getAllAvailable = (ids) => Accessory.find({_id: {$nin: ids}});
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
