4386089249663fc7d3f8b62526db42ff84a29568
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-08 17:31:10 +0200

    Update accessoryService.js

--- workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
+++ workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/services/accessoryService.js
@@ -2,6 +2,7 @@ const Accessory = require('../models/Accessory');
 
 exports.getAll = () => Accessory.find();
 // {$nin: ids} is an operator  -> Matches none of the values specified in an array.
+// give me Id that are not part of the ids that are given as a param
 exports.getAllAvailable = (ids) => Accessory.find({_id: {$nin: ids}});
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
