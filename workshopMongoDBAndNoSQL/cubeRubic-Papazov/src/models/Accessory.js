b76d1f6a29915c31e51e6ae8dbfbe4b2eb7f285f
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-08 15:25:35 +0200

    relations in mongoose

--- workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/models/Accessory.js
+++ workshopMongoDBAndNoSQL/cubeRubic-Papazov/src/models/Accessory.js
@@ -28,6 +28,10 @@ const accessorySchema = new mongoose.Schema({
             ref: 'Cube'
         }
     ]
+    // special field in document denoting the relation
+    // if it is an array, it is many to many, if it one object its obvious that only one relation is possible
+    // the 'type' has to be set to be OBJECT>ID
+    // the 'ref' has to be set to the relevant model 
 });
 
 const Accessory = mongoose.model('Accessory', accessorySchema);
