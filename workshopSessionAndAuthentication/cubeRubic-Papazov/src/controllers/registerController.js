faa16fe3d67023ba641d301d8c5b56f410a9e3a8
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-12 17:47:49 +0200

    explanation on circular dependancy and architercture from index to DB

--- workshopSessionAndAuthentication/cubeRubic-Papazov/src/controllers/registerController.js
+++ workshopSessionAndAuthentication/cubeRubic-Papazov/src/controllers/registerController.js
@@ -1,7 +1,23 @@
-// regsiter user and hashed pass
+// register user and hashed pass
+
+// very important on circular dependancy
+// if you make a sort of closure for userSession to be in index.js
+// and instead of passing it as an argument via routes and controllers
+// you export it and import it here in registerController which is a childNode of index.js
+// when you have to use userSession, the app will crash due to circular dependancy
+// index is exporting, meaning it is making registerController its parentNode
+
+// my view is that exports navigate you to the parentNode
+// index or app never have exports for that reason, they are usaully root
+// in this application, we have services used by more than one controller
+// but both services rely on DB that ends the node tree, cubeController uses
+// both cubeServices and accessory service while accessorry service is used
+// by accessoryController too, but there is no crash due to accessory relying on DB
+// that is why DB exports to services, services export to controllers, controllers export to routes, routes export to index
 
 const router = require('express').Router();
 const userSession = require('../data/db');
+
 const bcrypt = require('bcrypt');
 
 const saltRounds = 15;
