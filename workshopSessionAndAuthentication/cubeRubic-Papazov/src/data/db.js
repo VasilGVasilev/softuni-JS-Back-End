a8839a522ac71eba88b6a083d542c705aa4e1f84
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-12 13:07:33 +0200

    register controller
    check if user is in DB, if not hash password and set username and password in DB

--- workshopSessionAndAuthentication/cubeRubic-Papazov/src/data/db.js
+++ workshopSessionAndAuthentication/cubeRubic-Papazov/src/data/db.js
@@ -1,5 +1,7 @@
 const userSession = {};
 
+module.exports = userSession
+
 // reminder on const and let, you cannot change userSession to an array, string due to being
 // a const var, but how are you able to change the object, well, the userSession remains
 // an object {}, you just change the content {key: value} or {[],[],[]}
