48b5dd0918e84f6d5b2233802367ffb26215c5f9
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-18 17:28:46 +0200

    Update errorHandlerMIddleware.js

--- validation/cubeRubic-Papazov-full/src/middlewares/errorHandlerMIddleware.js
+++ validation/cubeRubic-Papazov-full/src/middlewares/errorHandlerMIddleware.js
@@ -2,5 +2,5 @@ exports.errorHandler = (err, req, res, next) => {
     const status = err.status || 404;
     const errorMessage = err.message || 'Something went wrong!';
     
-    res.status(status).render('404', { error: errorMessage });
+    res.status(status).render('404', { error: errorMessage }); //404 being in partial does not requires '/partials'
 };
