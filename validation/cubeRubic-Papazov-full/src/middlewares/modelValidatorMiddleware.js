56aeb8d6d359fd59e24e3758062e9ee82fe1afba
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-29 13:53:56 +0200

    function composition to input a value in middlware but also return a function that relies on this inputed value

--- validation/cubeRubic-Papazov-full/src/middlewares/modelValidatorMiddleware.js
+++ validation/cubeRubic-Papazov-full/src/middlewares/modelValidatorMiddleware.js
@@ -8,3 +8,13 @@ exports.modelValidator = (Model) => async (req, res, next) => {
         res.status(400).send(Object.values(error)[0]);
     }
 };
+// How does this work -> function composition, currying
+    // const curry = (a) => (b) => {a+b}
+
+    // JS creates a closure for the returned function '(b) => {a + b}' 
+    // that captures the value of 'a', thus, the returned function has
+    // access to 'a'.
+
+    // await Model.validate(req.body);
+    
+    // so the async function has access to Model as it is seen in:
