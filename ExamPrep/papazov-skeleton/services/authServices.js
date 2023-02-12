e0c8b497dea37d53b3f415f376b29749e21a90e0
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-12 17:10:12 +0200

    existing user validation

--- ExamPrep/papazov-skeleton/services/authServices.js
+++ ExamPrep/papazov-skeleton/services/authServices.js
@@ -1,14 +1,20 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
+exports.findByUsername = (username) => User.findOne({username});
+
 exports.register = async (username, email, password, repeatPassword) => {
 
     // Validate password with repeatPassword
     if(password !== repeatPassword) {
         throw new Error('Password mismatch')
     }
+
     // Check if user exists
-    // TODO
+    const existingUser = await this.findByUsername(username);
+    if(existingUser){
+        throw new Error('User exists')
+    }
 
     // Validate password here instead of in User model because you are passing in there a hashed pass, not the original! length, 
     // TODO
