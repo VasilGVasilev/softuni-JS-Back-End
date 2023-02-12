6a435ae9bd19a31c966258640e2fb9f3c8b50c67
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-12 14:47:42 +0200

    mongoose init

--- ExamPrep/papazov-skeleton/index.js
+++ ExamPrep/papazov-skeleton/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const { mongoose } = require('mongoose');
 
 const routes = require('./routes')
 
@@ -14,5 +15,8 @@ app.set('view engine', 'hbs');
 app.use('/static', express.static('public')) //tells to search in 'public' if you have '/static', optimised solution due to targeting not all request, rather only those with '/static'
 app.use(express.urlencoded({extended: false}))
 app.use(routes)
+
+mongoose.set('strictQuery', false); //deprication warning when starting app without it
+mongoose.connect('mongodb://localhost:27017/crypto')
 
 app.listen(5000, () => console.log('Server is running on port 5000...'))
